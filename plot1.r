3fc6bd8dcb9c77d3cbffb9e818f1d95a60d90f6b
jkklim <jkklim@hotmail.com>
2015-05-07 21:32:20 +0800

    Update plot1.r

--- plot1.r
+++ plot1.r
@@ -1,9 +1,7 @@
 Enter file contents here
-## read data, my household_power_consumption.txt is stored in e:\ ##
 ## plot1.R
 
-setwd("E:\\module4\\PROJECT")
-data <- read.table("household_power_consumption.txt", sep=";" , header=TRUE)
+data <- read.table("./data/household_power_consumption.txt", sep=";" , header=TRUE)
 subsetdata <- data[data$Date %in% c("1/2/2007", "2/2/2007"),]
 
 globalactivepower <- as.numeric(as.character(subsetdata$Global_active_power))
